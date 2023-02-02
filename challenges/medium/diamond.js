c1cb53f7a29fc9ddc658bd7b1a1b50999e1f37da
Brandon Corey <bcorey3660@gmail.com>
2023-02-01 19:58:45 -0500

    Refactors diamond problem

--- challenges/medium/diamond.js
+++ challenges/medium/diamond.js
@@ -1,76 +1,52 @@
-/* eslint-disable max-statements */
-/* eslint-disable max-lines-per-function */
-// diamond
+const Diamond = (() => {
+  const LETTER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
+  const outerSpaces = (letterIdx, charIdx) => {
+    return charIdx - letterIdx;
+  };
 
-// understand problem
-// width of each diamond
-// width gets two spaces wider with each additional letter
-//  - starts at 1 wide with 'A', 3 with 'B' etc.
-//  - A is always at the center idx of string (rounded up)
-//  - Each subsequent letter has two copies, one further left, and one further right by one space
-//  -each line ends with newline char
-
-// create template string for each loop that is correct width plus a newline char
-// if loop count = 0, place 'A' at midpoint
-// else place chart at midpoint - count and midpoint + count
-// loop until index of char in alphabet
-
-
-class Diamond {
-  static border = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  static makeDiamond(char) {
-    const charIdx = this.border.indexOf(char);
-    const width = (charIdx * 2) + 1;
-    const mid = Math.ceil(width / 2);
-    let spaceCnt = mid - 1;
-    let string = '';
-
-    for (let cnt = 0; cnt <= charIdx; cnt++) {
-      let letter = this.border[cnt];
-      let middleSpaces = this.middleSpaces(letter);
-      let spaces = ' '.repeat(spaceCnt);
-
-      if (cnt === 0) string += spaces + letter + spaces + '\n';
-      else {
-        string += spaces + letter + middleSpaces + letter + spaces + '\n';
-      }
-
-      spaceCnt -= 1;
-    }
-    spaceCnt = 1;
-
-    for (let cnt = charIdx - 1; cnt >= 0; cnt--) {
-      let letter = this.border[cnt];
-      let middleSpaces = this.middleSpaces(letter);
-      let spaces = ' '.repeat(spaceCnt);
-
-      if (cnt === 0) string += spaces + letter + spaces + '\n';
-      else {
-        string += spaces + letter + middleSpaces + letter + spaces + '\n';
-      }
-
-      spaceCnt += 1;
-    }
-    return string;
-  }
-
-  static middleSpaces(letter) {
-    let currLetterIdx = 1;
+  const middleSpaces = (char) => {
+    let currcharIdx = 1;
     let spaces = 1;
-    let currentLetter = this.border[currLetterIdx];
+    let letter = LETTER[currcharIdx];
 
-    if (letter === 'A') return '';
+    if (char === 'A') return '';
 
-    while (currentLetter !== letter) {
+    while (letter !== char) {
       spaces += 2;
-      currLetterIdx++;
-      currentLetter = this.border[currLetterIdx];
+      currcharIdx++;
+      letter = LETTER[currcharIdx];
     }
 
     return (' ').repeat(spaces);
-  }
-}
+  };
+
+  const buildDiamond = (letterIdx, charIdx) => {
+    let letter = LETTER[letterIdx];
+    let middle = middleSpaces(letter);
+    let spaces = ' '.repeat(outerSpaces(letterIdx, charIdx));
+    let string = '';
+
+    if (letterIdx === 0) string += spaces + letter + spaces + '\n';
+    else {
+      string += spaces + letter + middle + letter + spaces + '\n';
+    }
+    return string;
+  };
+
+  return class {
+    static makeDiamond(char) {
+      const charIdx = LETTER.indexOf(char);
+      let result = '';
+      for (let letterIdx = 0; letterIdx <= charIdx; letterIdx++) {
+        result += buildDiamond(letterIdx, charIdx);
+      }
+      for (let letterIdx = charIdx - 1; letterIdx >= 0; letterIdx--) {
+        result += buildDiamond(letterIdx, charIdx);
+      }
+      return result;
+    }
+  };
+})();
 
 module.exports = Diamond;
-console.log(Diamond.makeDiamond('E'));
